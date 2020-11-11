0a421da537a62f3e80e6d6b9edffbd2eff92a89f
Vitaliy Kovalenko <vit.kovalenko@icloud.com>
2020-11-11 16:26:18 +0200

    Update get-videos-list.js

--- api/videos/get-videos-list.js
+++ api/videos/get-videos-list.js
@@ -1,3 +1,9 @@
-export default function getVideosList(req, res) {
-	return res.sendStatus(204)
+import Video from './video-model.js'
+
+export default async function getVideosList(req, res) {
+	const { limit = 20, offset = 0 } = req.query
+
+	const videos = await Video.find().skip(parseInt(offset)).limit(parseInt(limit))
+
+	return res.send(videos)
 }
